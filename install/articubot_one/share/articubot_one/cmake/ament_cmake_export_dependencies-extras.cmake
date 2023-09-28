c8716eb16fe6f4a03cc4c830cd7aeb31a26c8073
danigorozz <13danielgomez@gmail.com>
2023-09-28 19:57:28 +0200

    Base móvil integrada en RVIZ (no Gazebo)

---
+++ install/articubot_one/share/articubot_one/cmake/ament_cmake_export_dependencies-extras.cmake
@@ -0,0 +1,1 @@
+/home/daniel/dev_ws/build/articubot_one/ament_cmake_export_dependencies/ament_cmake_export_dependencies-extras.cmake
---
+++ install/articubot_one/share/articubot_one/launch/rsp.launch.py
@@ -0,0 +1,1 @@
+/home/daniel/dev_ws/src/articubot_one/launch/rsp.launch.py
